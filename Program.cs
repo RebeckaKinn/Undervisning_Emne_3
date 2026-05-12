d3aaa619f60067be5de624c85d2251ec675f42cb
Rebecka Kinn <rebeckaki@getacademy.no>
2026-05-12 09:23:27 +0200

    Move sample code into Undervisning_dag_1.cs
    Refactor example/demo code: removed the interactive console add routine and associated examples from Program.cs and added a new file Undervisning_dag_1.cs. The new file contains the previous sample snippets and the AddNumbers method inside the Undervisning_Emne_3.Undervisning_dag_1 class, while Program.cs was simplified to a short comment listing lesson topics. This organizes exercises into a separate class file and keeps Program.cs as an outline.

--- Program.cs
+++ Program.cs
@@ -1,65 +1,11 @@
 ﻿
 
 /*
-string newName = "Rebecka";
-int number = 1;
-bool isTrue = true;
-float decimalNumber = 1.5F;
-double exstraNumber = 2.2;
-char symbol = '$';
-
-var newSymbol = 'K';
-*/
-//Console.Write(newName);
-//Console.WriteLine($"Hei {newName}");
-
-
-//var inputKey = Console.ReadKey();
-//Console.WriteLine($"inputKey: {inputKey.Key}");
-
-//var input = Console.Read();
-//Console.WriteLine($"input: {input}");
-
-
-
-
-//Console.WriteLine("Skriv inn navnet ditt:");
-//string input = ReadString();
-//PrintString(input);
-
-//void PrintString(string newInput)
-//{
-//    Console.WriteLine($"Hei {newInput}!");
-//}
-
-//string ReadString()
-//{
-//    return Console.ReadLine();
-//}
-
-
-
-
-
-
-//Console.WriteLine("Trykk på en tast:");
-//var key = ShowReadKey();
-//Console.Clear();
-//Console.WriteLine($"Du trykket på: {key.Key}");
-
-
-//ConsoleKeyInfo ShowReadKey()
-//{
-//    return Console.ReadKey();
-//}
-
-Console.WriteLine("Write a number:");
-var input1 = Console.ReadLine();
-Console.WriteLine(" + ");
-var input2 = Console.ReadLine();
-Console.WriteLine($"= {AddNumbers(Convert.ToDouble(input1), Convert.ToDouble(input2))}");
-
-double AddNumbers(double number1, double number2)
-{
-    return number1 + number2;
-}
+ * Klasser
+ * Instanse av en klasse (kopi)
+ * "Hoppe ut av" program.cs
+ * if/else og switch
+ * Lage meny
+ * Random rand = new Random();
+ * Loop: While(){}
+ */
---
+++ Undervisning_dag_1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Undervisning_Emne_3
+{
+    internal class Undervisning_dag_1
+    {
+
+
+        /*
+        string newName = "Rebecka";
+        int number = 1;
+        bool isTrue = true;
+        float decimalNumber = 1.5F;
+        double exstraNumber = 2.2;
+        char symbol = '$';
+
+        var newSymbol = 'K';
+        */
+        //Console.Write(newName);
+        //Console.WriteLine($"Hei {newName}");
+
+
+        //var inputKey = Console.ReadKey();
+        //Console.WriteLine($"inputKey: {inputKey.Key}");
+
+        //var input = Console.Read();
+        //Console.WriteLine($"input: {input}");
+
+
+
+
+        //Console.WriteLine("Skriv inn navnet ditt:");
+        //string input = ReadString();
+        //PrintString(input);
+
+        //void PrintString(string newInput)
+        //{
+        //    Console.WriteLine($"Hei {newInput}!");
+        //}
+
+        //string ReadString()
+        //{
+        //    return Console.ReadLine();
+        //}
+
+
+
+
+
+
+        //Console.WriteLine("Trykk på en tast:");
+        //var key = ShowReadKey();
+        //Console.Clear();
+        //Console.WriteLine($"Du trykket på: {key.Key}");
+
+
+        //ConsoleKeyInfo ShowReadKey()
+        //{
+        //    return Console.ReadKey();
+        //}
+
+        //Console.WriteLine("Write a number:");
+        //var input1 = Console.ReadLine();
+        //Console.WriteLine(" + ");
+        //var input2 = Console.ReadLine();
+        //Console.WriteLine($"= {AddNumbers(Convert.ToDouble(input1), Convert.ToDouble(input2))}");
+
+        double AddNumbers(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+    }
+}
