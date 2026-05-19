185f0fce7913e45fbb5469de055f137eb298aa87
Rebecka Kinn <rebeckaki@getacademy.no>
2026-05-19 08:55:32 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,11 @@
 ﻿
-using Undervisning_Emne_3;
-
-var game = new Game();
-game.Run();
 
 
 
 /*
- * Klasser
- * Instanse av en klasse (kopi)
- * "Hoppe ut av" program.cs
- * if/else og switch
- * Lage meny
- * Random rand = new Random();
- * Loop: While(){}
+ * Klasser og properties
+ * Constructor
+ * Klasser som mal til objekter
+ * Liste av objekter
+ * Prosjekt - Kontaktinformasjonsoppgaven (Litt som sist, men bruker objekter istedenfor)
  */
